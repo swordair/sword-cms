c684e8db8ebd849e5b1b4f2599e88b34e55495c7
swordair <iifksp@swordair.com>
2014-07-09 17:41:47 +0800

    add font-awesome.css

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-/// error handlers
+// error handlers
 
 // development error handler
 // will print stacktrace
