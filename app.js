b9bfed107d580b2ccd11d98ceb5e5fa69dcf5ac4
swordair <iifksp@swordair.com>
2014-07-09 22:06:30 +0800

    add passport, mongoose
    add passport, passport-local, connect-flash, mongoose, express-session

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var hbs = require('express-hbs');
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 
 var routes = require('./routes/index');
@@ -26,10 +33,30 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({secret: 'my name is sword'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
+
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+mongoose.connect('mongodb://localhost/sword_cms');
+
+
+
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +64,21 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // error handlers
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var Account = require('../models/account');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Sword CMS' });
+
+router.get('/', function(req, res){
+
+	if(req.user){
+		res.render('index', {title: 'Sword CMS'});
+	}
+	else{
+		res.redirect('/login');
+	}
+
+	
 });
+
+
+router.get('/register', function(req, res) {
+	res.render('register', {title: 'Sword CMS register'});
+});
+
+
+router.post('/register', function(req, res){
+	Account.register(new Account({username : req.body.username}), req.body.password, function(err, account){
+		if(err){
+			return res.render('register', {err_msg : err.message});
+		}
+		console.log(account);
+		passport.authenticate('local')(req, res, function(){
+			res.redirect('/');
+		});
+	});
+});
+
+
+router.get('/login', function(req, res){
+	res.render('login', {msg:req.flash("error")});
+});
+
+
+router.post('/login', passport.authenticate('local',{ successRedirect: '/',
+                                   failureRedirect: '/login',
+								   failureFlash: 'Invalid username or password.' }), function(req, res) {
+	res.redirect('/');
+});
+
+
+router.get('/logout', function(req, res){
+	req.logout();
+	res.render('logout', {});
+});
+
+
 
 module.exports = router;
